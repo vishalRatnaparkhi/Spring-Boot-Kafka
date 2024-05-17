e741f8a6861184c13946e4ae2050b4db6af78522
Vishal Ratnaparkhi <ratnparkhiv@gmail.com>
2024-05-17 19:29:57 +0530

    First Commit in EndUser App

---
+++ src/test/java/com/kafka/EndUser/EndUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.EndUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
